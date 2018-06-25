1f1f04272bbc2a525876627a0e64577c7d58f8e3
ayo wynter <ayo.wynter@yahoo.com>
2018-06-25 18:28:36 -0400

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,34 @@
+ var React = require('react');
+ var ReactDOM = require('react-dom');
+ var {Route, Router,IndexRoute,hashHistory} = require('react-router');
+
+var Main = require('Main');
+
+
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
+//App css
+require('style!css!sass!applicationStyles')
+
+
+
+ReactDOM.render(
+   <Router history={hashHistory}>
+   <Route path = '/' component={Main}>
+
+   </Route>
+   </Router>,
+    document.getElementById('app')
+  );
+
+
+//61d10c4dc62e28ce7e833eed8e5f1d4c
+//https://api.darksky.net/forecast/61d10c4dc62e28ce7e833eed8e5f1d4c/37.8267,-122.4233
+// https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+//6b17e9067a4a972c2f9dd49bfaa2782a
+// cheap-module-eval-source-map
+// inline-source-map
+// eval-source-map
+
---
+++ app/components/Navigation.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () =>{
+  return(
+    <div className="top-bar">
+    <div className="top-bar-left">
+    <ul className="menu">
+    <li className="menu-text">
+    React Timer App
+    </li>
+    <li>
+    <Link to="/" activeClassName = "active-link">Timer</Link>
+    </li>
+    <li>
+    <Link to="/" activeClassName = "active-link">Countdown</Link>
+    </li>
+    </ul>
+    </div>
+    <div className="top-bar-right">
+    <ul className="menu">
+    <li>
+    Created by <a href="http://www.mead.io" target="_blank">Andrew Mead</a>
+    </li>
+    </ul>
+    </div>
+    </div>)
+}
+
+module.exports = Navigation;
