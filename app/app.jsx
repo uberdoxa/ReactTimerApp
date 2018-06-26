63700966d276ff333462dd6e636c3405bddf3bf5
ayo wynter <ayo.wynter@yahoo.com>
2018-06-26 16:57:34 -0400

    Add navigation

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,9 @@
- var React = require('react');
- var ReactDOM = require('react-dom');
- var {Route, Router,IndexRoute,hashHistory} = require('react-router');
-
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router,IndexRoute,hashHistory} = require('react-router');
 var Main = require('Main');
-
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css')
@@ -12,23 +12,13 @@ $(document).foundation();
 //App css
 require('style!css!sass!applicationStyles')
 
-
-
 ReactDOM.render(
    <Router history={hashHistory}>
    <Route path = '/' component={Main}>
-
+    <Route path = "countdown" component={Countdown}/>
+    <IndexRoute component = {Timer}/>
    </Route>
    </Router>,
     document.getElementById('app')
   );
-
-
-//61d10c4dc62e28ce7e833eed8e5f1d4c
-//https://api.darksky.net/forecast/61d10c4dc62e28ce7e833eed8e5f1d4c/37.8267,-122.4233
-// https://api.darksky.net/forecast/[key]/[latitude],[longitude]
-//6b17e9067a4a972c2f9dd49bfaa2782a
-// cheap-module-eval-source-map
-// inline-source-map
-// eval-source-map
 
---
+++ app/components/Timer.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render: function(){
+    return(
+      <p>Timer component</p>)
+  }
+
+});
+
+module.exports = Timer;
